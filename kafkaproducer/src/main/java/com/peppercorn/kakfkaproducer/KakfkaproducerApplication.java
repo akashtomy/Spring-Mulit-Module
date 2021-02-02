bf1973b0e20c1007429c8835dea22e426c858898
Akash Thomas <aakashthebraveman@gmail.com>
2021-02-02 17:18:12 +0530

    intial commit

---
+++ kafkaproducer/src/main/java/com/peppercorn/kakfkaproducer/KakfkaproducerApplication.java
@@ -0,0 +1,13 @@
+package com.peppercorn.kakfkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakfkaproducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakfkaproducerApplication.class, args);
+	}
+
+}
---
+++ kafkaproducer/src/test/java/com/peppercorn/kakfkaproducer/KakfkaproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peppercorn.kakfkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakfkaproducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
